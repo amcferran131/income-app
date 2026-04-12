569ccdd598c9b46f81accab98ad25d7a8ed562a8
amcferran131 <amcferran@gmail.com>
2026-04-11 21:42:27 -0400

    Update api.py

--- api.py
+++ api.py
@@ -8,12 +8,12 @@ from collections import Counter
 
 app = Flask(__name__)
 
-ALLOWED_ORIGIN = 'https://dividend-calculator-blond.vercel.app'
+ALLOWED_ORIGINS = ['https://dividend-calculator-blond.vercel.app', 'https://dividend-finder.vercel.app']
 
 @app.after_request
 def add_cors(response):
     origin = request.headers.get('Origin', '')
-    if origin == ALLOWED_ORIGIN:
+    if origin in ALLOWED_ORIGINS:
         response.headers['Access-Control-Allow-Origin'] = origin
         response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
